bd02dd56a53a35f635e5a574a867ed1f702a77ca
NicolasMafla <49218831+NicolasMafla@users.noreply.github.com>
2023-04-30 20:50:44 -0500

    Box plot added

--- index.js
+++ index.js
@@ -7,44 +7,38 @@ const appDiv = document.getElementById('app');
 
 
 d3.csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv", function (error, data) {
-
     var visualization = d3plus.viz()
-
         .container("#viz1")
-
         .data([{"year": 1991, "name": "alpha", "value": 15}, {"year": 1992, "name": "alpha", "value": 20},])
-
         .type("bar")
-
         .id("name")
-
         .x("year")
-
         .y("value")
-
         .draw()
 
 });
 
 
 d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-Visualizacion-Actividades-Guiadas/master/columnas.json", function (data) {
-
     var visualization = d3plus.viz()
-
         .container("#viz2")
-
         .data(data)
-
         .type('bar')
-
         .id('name')
-
         .x('year')
-
         .y('value')
-
         .axes({ticks: 'false'})
-
         .draw();
 
+});
+
+d3.json("https://raw.githubusercontent.com/NicolasMafla/08MBID---Visualizacion-de-Datos/ag3/data.json", function (data) {
+    var visualization = d3plus.viz()
+        .container("#viz3")
+        .data(data)
+        .type('box')
+        .id('name')
+        .x('year')
+        .y('value')
+        .draw();
 });
